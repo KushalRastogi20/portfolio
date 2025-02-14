b880957093a417e838cac1fab885c6f126181309
KushalRastogi20 <138278696+KushalRastogi20@users.noreply.github.com>
2025-02-15 00:35:21 +0530

    Hero section

--- kushal-profile/src/app/page.js
+++ kushal-profile/src/app/page.js
@@ -3,93 +3,38 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className={styles.main}>
+      <nav className={styles.nav}>
+        {/* <h1 className={styles.title}>Kushal Rastogi</h1>   */}
+        <ul className={styles.navList}>
+          {/* <li className={styles.navItem}>Kushal Rastogi</li> */}
+          <li className={styles.navItem}>About</li>
+          <li className={styles.navItem}>Projects</li>
+          <li className={styles.navItem}>Skills</li>
+          <li className={styles.navItem}>Contact</li>
+        </ul>
+        
+      </nav>
+        <h1 className={styles.title}>Kushal Rastogi</h1>
+        <h2 className={styles.subtitle}>Full Stack Developer</h2>
+        <h3 className={styles.description}>
+          I am a full stack developer building scalable web applications.
+         I have worked on various technologies like Next, React, Node, Express, MongoDB, etc.
+         </h3>
+        <h2 className={styles.subtitle}>Algorithm Engineer & Competitive Programmer </h2>
+        <h3 className={styles.description}> Experienced in designing and optimizing real-world algorithms as an Algorithm Engineer,
+         while excelling in solving complex problems quickly through competitive programming using Java.</h3>
+        <div className={styles.image}>
+          <Image
+            src="/kushal.jpg"
+            alt="Kushal Rastogi"
+            width={200}
+            height={200}
+            className={styles.image}
+          />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <h2 className={styles.subtitle}>Projects</h2>
+        
     </div>
-  );
+  );  
 }
